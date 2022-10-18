1d64668b41639187817b09164738a71515ecca66
victoriamelo09 <vitoria.melo0901@hotmail.com>
2022-10-18 14:50:58 -0300

    adicionando testes

--- blogpessoal/src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -15,35 +15,44 @@ import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-
 @Entity
 @Table(name = "tb_usuarios")
 public class Usuario {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id; 
-	
+	private Long id;
+
 	@NotBlank(message = "O atributo nome é obrigatório!")
-	private String nome; 
-	
+	private String nome;
+
 	@NotBlank(message = "O atributo usuário é obrigatório!")
 	@Email(message = "É necessário que o e-mail seja válido")
-	private String usuario; 
-	
+	private String usuario;
+
 	@NotBlank(message = "O atributo é obrigatório")
 	@Size(min = 8, message = "A senha deve conter no mínimo 8 caracteres")
-	private String senha; 
-	
+	private String senha;
+
 	@Size(max = 5000, message = "O link da foto não pode ser maior do que 5000 caracteres")
 	private String foto;
-	
+
 	@OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE)
 	@JsonIgnoreProperties("usuario")
-	private List <Postagem> postagem;
+	private List<Postagem> postagem;
 
-	
-	
+	public Usuario(Long id, String nome, String usuario, String senha, String foto) {
+
+		this.id = id;
+		this.nome = nome;
+		this.usuario = usuario;
+		this.senha = senha;
+		this.foto = foto;
+
+	}
+
+	public Usuario() { }
+
 	public Long getId() {
 		return id;
 	}
@@ -91,8 +100,5 @@ public class Usuario {
 	public void setPostagem(List<Postagem> postagem) {
 		this.postagem = postagem;
 	}
-	
-
-		
 
 }
--- blogpessoal/src/main/java/com/generation/blogpessoal/repository/UsuarioRepository.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/repository/UsuarioRepository.java
@@ -1,5 +1,6 @@
 package com.generation.blogpessoal.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,4 +13,7 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 	
 	public Optional<Usuario> findByUsuario(String usuario);
 
+	public List <Usuario> findAllByNomeContainingIgnoreCase(String nome);
+
+	
 }
